b26246d0e07f7912d9275b80f9078c2bedd8fab4
MOHAN KAMAL HASSAN <34089649+mohankamal@users.noreply.github.com>
2024-11-13 09:50:19 -0500

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Mohan Kamal H";
+            document.title = "Portfolio | Mohan Kamal";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
